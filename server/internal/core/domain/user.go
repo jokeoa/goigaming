eec8d71e69111694cdee6964cd565b28460c9d07
Hanulus <chingizuraimov@gmail.com>
2026-02-11 14:39:28 +0500

    Update user.go

--- server/internal/core/domain/user.go
+++ server/internal/core/domain/user.go
@@ -11,6 +11,7 @@ type User struct {
 	Username     string    `json:"username"`
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"-"`
+	RefreshToken *string   `json:"-"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
